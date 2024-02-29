6f77fe8c51e26a7c80b5dc8904b76f875e103940
SyedFazal110 <syedfazal808@gmail.com>
2024-02-29 22:17:09 +0500

    index file created

---
+++ file.js
@@ -0,0 +1,34 @@
+var number = +prompt("Enter Your Marks");
+if(number >100 ){
+    console.log("Enter your correct Marks")
+}
+else if(number >=90 && number <=100 ){
+    console.log("A++")
+}
+else if(number >=80 && number <=89 ){
+    console.log("Grade A1")
+}
+else if(number >=70 && number <=79 ){
+    console.log("Grade A")
+}
+else if(number >=60 && number <=69 ){
+    console.log("Grade B")
+}
+else if(number >=50 && number <=59 ){
+    console.log("Grade C")
+}
+else if(number >=40 && number <=49 ){
+    console.log("Grade D")
+}
+else {
+    console.log("You Failed")
+}
+
+
+// var num = 461;
+// if(num % 2 == 0){
+//     console.log("number is divisible by 2");
+// }
+// else{
+//     console.log("number is not divisible by 2")
+// }
